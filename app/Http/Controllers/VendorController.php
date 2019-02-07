c02f081a47075754d17b02b813d3d1265220fc2a
shrestsav <shrestsav@gmail.com>
2019-02-07 21:55:49 +0545

    Middleware group

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -12,6 +12,11 @@ class VendorController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
             
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('vendors','VendorController');
-Route::resource('buyers','BuyerController');
+
+Route::middleware(['auth'])->group(function () {
+	Route::resource('vendors','VendorController');
+	Route::resource('buyers','BuyerController');
+});
